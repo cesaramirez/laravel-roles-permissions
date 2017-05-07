2b76931d9266c2fa9e17f4f19a165cacb143a6e3
César Ramírez <cesar.ramirez.sv@gmail.com>
2017-05-06 21:07:54 -0600

    Add middleware to roles

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,18 @@ Route::get('/', function (\Illuminate\Http\Request $request) {
     $user = $request->user();
 
     if ( $user ) {
-        dump($user->withdrawPermissionTo(['edit posts']));      
+        dump($user->withdrawPermissionTo(['edit posts']));
     }
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', function () {
+    return 'admin';
+})->middleware('role:admin');
+
+Route::get('/admin/users', function () {
+    return 'admin users';
+})->middleware('role:admin,delete users');
